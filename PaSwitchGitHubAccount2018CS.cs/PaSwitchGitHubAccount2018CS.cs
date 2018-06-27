4b0df008ce46b0379d5e393920f552acd95d10fc
asm32cn <asm32@qq.com>
2018-06-28 02:57:47 +0800

    PaSwitchGitHubAccount2018CS.cs ... modify

--- PaSwitchGitHubAccount2018CS.cs/PaSwitchGitHubAccount2018CS.cs
+++ PaSwitchGitHubAccount2018CS.cs/PaSwitchGitHubAccount2018CS.cs
@@ -1,8 +1,12 @@
 // PaSwitchGitHubAccount2018CS.cs
 
 using System;
+using System.IO;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Text;
+using System.Security.Cryptography;
+
 
 class PaSwitchGitHubAccount2018CS:Form{
 	private TableLayoutPanel tlp = new TableLayoutPanel();
@@ -10,14 +14,42 @@ class PaSwitchGitHubAccount2018CS:Form{
 	private const int nCommandCount = 4;
 	private Button[] btnCommand = new Button[nCommandCount];
 
+	private string strFolderUserProfile = Environment.GetEnvironmentVariable("USERPROFILE") + Path.DirectorySeparatorChar;
+
 	protected override Size DefaultSize {
 		get{ return new Size(300, 300); }
+	}
+
+	public string ReadTextFile(string strFile){
+		string strContent = null;
+		try{
+			using(FileStream fs = new FileStream(strFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)){
+				using(StreamReader sr = new StreamReader(fs, System.Text.Encoding.UTF8)){
+					strContent =  sr.ReadToEnd();
+				}
+			}
+		}catch(Exception ex){
+			MessageBox.Show("Exception: " + ex.Message);
+		}
+		return strContent;
 	}
 
 	public PaSwitchGitHubAccount2018CS(){
 		this.Text = "PaSwitchGitHubAccount2018CS.cs";
 		this.StartPosition = FormStartPosition.CenterScreen;
 		// this.MinimumSize = new Size(400, 450);
+
+		Console.WriteLine( "USERPROFILE = " + strFolderUserProfile );
+
+		string strGitConfig = ReadTextFile(strFolderUserProfile + ".gitconfig");
+		string strEncrypt = AESEncrypt( strGitConfig );
+		
+		// Console.WriteLine(strGitConfig);
+		Console.WriteLine( strEncrypt );
+
+		strGitConfig = ReadTextFile(strFolderUserProfile + ".git-credentials");
+		strEncrypt = AESEncrypt( strGitConfig );
+		Console.WriteLine( strEncrypt );
 
 		initUI();
 	}
@@ -35,17 +67,12 @@ class PaSwitchGitHubAccount2018CS:Form{
 			this.tlp.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
 		}
 
-		Size size1 = this.ClientSize;
-		int nClientWidth = size1.Width;
-		// MessageBox.Show("size1.x" + size1.Width);
 		for(int i = 0; i < nCommandCount; i++){
 			btnCommand[i] = new Button();
-			btnCommand[i].Text = "asm32cn@github.com";
-			btnCommand[i].Size = new Size(nClientWidth - 10, 40);
-			btnCommand[i].Location = new Point(5, i * 50 + 5 );
-
-			btnCommand[i].Dock = DockStyle.Fill;
 			btnCommand[i].Anchor = as1;
+			btnCommand[i].Dock = DockStyle.Fill;
+
+			btnCommand[i].Text = "asm32cn@github.com";
 
 			// btnCommand[i].Click += delegate(object sender, EventArgs e){};
 			btnCommand[i].Click += new EventHandler(btnCommand_OnClick);
@@ -77,6 +104,30 @@ class PaSwitchGitHubAccount2018CS:Form{
 			break;
 		}
 	}
+
+	public static byte[] _key1 = { 0xc4, 0xfe, 0xbe, 0xb2, 0xd6, 0xc2, 0xd4, 0xb6, 0xb5, 0xad, 0xb2, 0xb4, 0xc3, 0xf7, 0xd6, 0xbe }; // 宁静致远淡泊明志
+	public static string keys = "1a2s3d4f5g6h7j8k";//密钥,128位  
+
+	public string AESEncrypt(string plainText){
+			SymmetricAlgorithm des = Rijndael.Create();
+			byte[] inputByteArray = Encoding.UTF8.GetBytes(plainText);		
+			des.Key = Encoding.UTF8.GetBytes(keys);
+			des.IV = _key1;
+
+			ICryptoTransform cTransform = des.CreateEncryptor();
+			return Convert.ToBase64String( cTransform.TransformFinalBlock(inputByteArray, 0, inputByteArray.Length) );
+	}
+
+	public string AESDecrypt(string cipherText){
+			SymmetricAlgorithm des = Rijndael.Create();
+			des.Key = Encoding.UTF8.GetBytes(keys);
+			des.IV = _key1;
+
+			ICryptoTransform cTransform = des.CreateDecryptor();
+			byte[] data = Convert.FromBase64String(cipherText);
+			return System.Text.Encoding.UTF8.GetString( cTransform.TransformFinalBlock(data, 0, data.Length) );
+	}
+
 
 	public static void Main(string[] args){
 		Application.Run(new PaSwitchGitHubAccount2018CS());
