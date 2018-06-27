64777156d5b81fd20755693f8cedf69f9ef14af3
asm32cn <asm32@qq.com>
2018-06-27 13:29:19 +0800

    PaSwitchGitHubAccount2018CS--linker.bat ... add

---
+++ PaSwitchGitHubAccount2018CS.cs/PaSwitchGitHubAccount2018CS--linker.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set strLinker=%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\csc.exe
+set strAppName=PaSwitchGitHubAccount2018CS
+
+echo %strLinker%
+echo.
+
+rem %strLinker% /target:winexe /resource:res\%strAppName%.resources /win32icon:res\%strAppName%.ico /optimize %strAppName%.cs
+%strLinker% /target:winexe /optimize %strAppName%.cs
+
+pause
