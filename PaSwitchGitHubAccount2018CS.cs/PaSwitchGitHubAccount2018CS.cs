ed4599301361507109adfd184a8c0129a12b2423
asm32cn <asm32@qq.com>
2018-06-27 22:49:01 +0800

    PaSwitchGitHubAccount2018CS.cs ... modify

--- PaSwitchGitHubAccount2018CS.cs/PaSwitchGitHubAccount2018CS.cs
+++ PaSwitchGitHubAccount2018CS.cs/PaSwitchGitHubAccount2018CS.cs
@@ -5,11 +5,13 @@ using System.Drawing;
 using System.Windows.Forms;
 
 class PaSwitchGitHubAccount2018CS:Form{
+	private TableLayoutPanel tlp = new TableLayoutPanel();
+	private AnchorStyles as1 = ((AnchorStyles)((((AnchorStyles.Top | AnchorStyles.Bottom) | AnchorStyles.Left) | AnchorStyles.Right)));
 	private const int nCommandCount = 4;
 	private Button[] btnCommand = new Button[nCommandCount];
 
 	protected override Size DefaultSize {
-		get{ return new Size(300, 450); }
+		get{ return new Size(300, 300); }
 	}
 
 	public PaSwitchGitHubAccount2018CS(){
@@ -21,17 +23,40 @@ class PaSwitchGitHubAccount2018CS:Form{
 	}
 
 	public void initUI(){
-		Size size1 = this.Size;
+		this.tlp.SuspendLayout();
+		this.SuspendLayout();
+
+		this.tlp.Anchor = as1;
+		this.tlp.ColumnCount = 1;
+		this.tlp.RowCount = 4;
+		this.tlp.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+
+		for(int i = 0; i < nCommandCount; i++){
+			this.tlp.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+		}
+
+		Size size1 = this.ClientSize;
 		int nClientWidth = size1.Width;
 		// MessageBox.Show("size1.x" + size1.Width);
 		for(int i = 0; i < nCommandCount; i++){
 			btnCommand[i] = new Button();
 			btnCommand[i].Text = "asm32cn@github.com";
-			btnCommand[i].Size = new Size(nClientWidth - 20, 40);
+			btnCommand[i].Size = new Size(nClientWidth - 10, 40);
 			btnCommand[i].Location = new Point(5, i * 50 + 5 );
 
-			Controls.Add(btnCommand[i]);
+			btnCommand[i].Dock = DockStyle.Fill;
+			btnCommand[i].Anchor = as1;
+
+			this.tlp.Controls.Add(btnCommand[i], 0, i);
 		}
+
+		this.Controls.Add(this.tlp);
+		this.Padding = new Padding(10);
+		this.tlp.ResumeLayout(false);
+		this.tlp.PerformLayout();
+		this.ResumeLayout(false);
+		this.tlp.Dock = DockStyle.Fill;
+
 	}
 
 	public static void Main(string[] args){
